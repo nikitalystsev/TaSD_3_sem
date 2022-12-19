7e799c27025f8387bc8448d4bea5821bfb892609
lnd21u288 <lyystsevnd@student.bmstu.ru>
2022-12-19 12:31:52 +0300

    add files

--- labs/lab_08/inc/graph.h
+++ labs/lab_08/inc/graph.h
@@ -9,19 +9,19 @@ typedef struct adj_list_t adj_list_t;
 
 struct adj_list_t
 {
-    int number;
+    int number; // номер вершины
     adj_list_t *next;
 };
 struct vertex_t
 {
-    int number;
-    int degree;
-
+    int number;       // номер вершины
+    int degree;       // степень вершины
+    adj_list_t *head; // список смежности данной вершины
 };
 
 struct graph_t
 {
-    int number;
+    vertex_t *data; // массив  вершин
 };
 
 #endif
