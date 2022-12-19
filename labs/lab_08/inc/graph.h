56adf88e3677ba633c8441dfd0131e31e58db5ad
lnd21u288 <lyystsevnd@student.bmstu.ru>
2022-12-19 12:26:55 +0300

    add files

--- labs/lab_08/inc/graph.h
+++ labs/lab_08/inc/graph.h
@@ -3,4 +3,25 @@
 
 #include "defines.h"
 
+typedef struct graph_t graph_t;
+typedef struct vertex_t vertex_t;
+typedef struct adj_list_t adj_list_t;
+
+struct adj_list_t
+{
+    int number;
+    adj_list_t *next;
+};
+struct vertex_t
+{
+    int number;
+    int degree;
+
+};
+
+struct graph_t
+{
+    int number;
+};
+
 #endif
